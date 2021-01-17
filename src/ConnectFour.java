a8a2cc58508a8adc1ea664f10651346da544427e
Setomaster <afarah500@gmail.com>
2021-01-17 15:21:34 -0500

    Working game, add horizontal and vertical check

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,29 +1,36 @@
+import java.util.Scanner;
+
 public class ConnectFour {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         ConnectFourGame game = new ConnectFourGame();
-        System.out.println(game.gameState);
+        while (game.getGameState() == GameState.PLAYING) {
+            System.out.println(game);
+            System.out.println();
+            switch (game.nextCellValue()) {
+                case RED -> System.out.println("X turn");
+                case YELLOW -> System.out.println("O turn");
+            }
+            CellValue current = game.nextCellValue();
+            do {
+                try {
+                    int move = Integer.parseInt(scanner.next());
+                    game.play(move - 1);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Choose position between 1-7.");
+                } catch (IllegalStateException e) {
+                    System.out.println("Column is full.");
+                }
+            } while (current == game.nextCellValue());
+        }
         System.out.println(game);
-        game.play(3);
-        System.out.println(game.gameState);
-        System.out.println(game);
-        game.play(6);
-        System.out.println(game.gameState);
-        System.out.println(game);
-        game.play(0);
-        System.out.println(game.gameState);
-        System.out.println(game);
-        game.play(6);
-        System.out.println(game.gameState);
-        System.out.println(game);
-        game.play(1);
-        System.out.println(game.gameState);
-        System.out.println(game);
-        game.play(6);
-        System.out.println(game.gameState);
-        System.out.println(game);
-        game.play(2);
-        System.out.println(game.gameState);
-        System.out.println(game);
+        System.out.println();
+        switch (game.getGameState()) {
+            case RED_WIN -> System.out.println("X wins!");
+            case YELLOW_WIN -> System.out.println("O wins!");
+            case DRAW -> System.out.println("Draw");
+        }
     }
 }
--- src/ConnectFourGame.java
+++ src/ConnectFourGame.java
@@ -1,10 +1,8 @@
 public class ConnectFourGame {
 
     private final CellValue[] board;
-
+    private GameState gameState;
     private int level;
-
-    public GameState gameState;
 
     public ConnectFourGame() {
         board = new CellValue[42];
@@ -18,9 +16,9 @@ public class ConnectFourGame {
         return (level % 2 == 0) ? CellValue.RED : CellValue.YELLOW;
     }
 
-    public boolean play(int m) {
+    public void play(int m) {
         if (m < 0 || m > 6) {
-            throw new IllegalStateException("Illegal position");
+            throw new IllegalArgumentException("Illegal position");
         }
         if (gameState != GameState.PLAYING) {
             throw new IllegalStateException("Game finished");
@@ -30,57 +28,61 @@ public class ConnectFourGame {
                 board[m + (i * 7)] = nextCellValue();
                 updateGameState(m + (i * 7));
                 level++;
-                return true;
+                return;
             }
         }
-        return false;
+        throw new IllegalStateException("Column is full");
     }
 
-    public void updateGameState(int m) {
-        //FIXME
-        //TODO
-        if (isFull()) {
+    private void updateGameState(int m) {
+
+        if (level == 42) {
             gameState = GameState.DRAW;
             return;
         }
-        int r = (int) Math.ceil((double) m / 7);
 
-        if (m + 3 <= (r * 7 - 1)) {
-            int c = 0;
-            for (int i = m + 1; i < m + 4; i++) {
-                if (board[i] == board[m]) {
-                    c++;
-                }
-                else{
-                    break;
-                }
+        int r = (int) Math.ceil((double) (m + 1) / 7);
+        int count = 0;
+
+        //Check horizontal
+        for (int i = (r - 1) * 7; i < r * 7; i++) {
+            if (board[i] == board[m]) {
+                count++;
+            } else {
+                count = 0;
             }
-            if (c == 3) {
+            if (count == 4) {
                 setWinner(board[m]);
                 return;
             }
         }
 
-        if (m - 3 >= ((r - 1) * 7)) {
-            int c = 0;
-            for (int i = m - 1; i > m - 4; i--) {
-                if (board[i] == board[m]) {
-                    c++;
-                }
-                else{
-                    break;
-                }
+        int c = (m + 1) % 7;
+        if (c == 0) {
+            c = 7;
+        }
+        count = 0;
+
+        //Check vertical
+        for (int i = c - 1; i < (c - 1) + 36; i = i + 7) {
+            if (board[i] == board[m]) {
+                count++;
+            } else {
+                count = 0;
             }
-            if (c == 3) {
+            if (count == 4) {
                 setWinner(board[m]);
                 return;
             }
         }
 
+        //TODO
+        //Check diagonal
     }
 
     public String toString() {
         StringBuilder res = new StringBuilder();
+        res.append("1 2 3 4 5 6 7");
         for (int i = 0; i < 42; i++) {
             if (i % 7 == 0) {
                 res.append("\n");
@@ -98,20 +100,15 @@ public class ConnectFourGame {
         return res.toString();
     }
 
-    public boolean isFull() {
-        for (int i = 0; i < 42; i++) {
-            if (board[i] == CellValue.EMPTY) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void setWinner(CellValue cell) {
+    private void setWinner(CellValue cell) {
         if (cell == CellValue.RED) {
             gameState = GameState.RED_WIN;
         } else {
             gameState = GameState.YELLOW_WIN;
         }
+    }
+
+    public GameState getGameState() {
+        return gameState;
     }
 }
